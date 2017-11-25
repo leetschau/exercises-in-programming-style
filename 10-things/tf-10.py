b434f81bcd0b521e1a751a3863459d5bec5c2f70
Li Chao <leetschau@gmail.com>
2017-11-25 23:30:48 +0800

    format codes according to PEP 8

--- 10-things/tf-10.py
+++ 10-things/tf-10.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-import sys, re, operator, string
+import sys
+import re
+import operator
+import string
 from abc import ABCMeta
+
 
 #
 # The classes
@@ -11,9 +15,10 @@ class TFExercise():
     def info(self):
         return self.__class__.__name__
 
+
 class DataStorageManager(TFExercise):
     """ Models the contents of the file """
-    
+
     def __init__(self, path_to_file):
         with open(path_to_file) as f:
             self._data = f.read()
@@ -25,11 +30,14 @@ class DataStorageManager(TFExercise):
         return self._data.split()
 
     def info(self):
-        return super(DataStorageManager, self).info() + ": My major data structure is a " + self._data.__class__.__name__
+        return (super(DataStorageManager, self).info() +
+                ": My major data structure is a " +
+                self._data.__class__.__name__)
+
 
 class StopWordManager(TFExercise):
     """ Models the stop word filter """
-    
+
     def __init__(self):
         with open('../stop_words.txt') as f:
             self._stop_words = f.read().split(',')
@@ -40,11 +48,14 @@ class StopWordManager(TFExercise):
         return word in self._stop_words
 
     def info(self):
-        return super(StopWordManager, self).info() + ": My major data structure is a " + self._stop_words.__class__.__name__
+        return (super(StopWordManager, self).info() +
+                ": My major data structure is a " +
+                self._stop_words.__class__.__name__)
+
 
 class WordFrequencyManager(TFExercise):
     """ Keeps the word frequency data """
-    
+
     def __init__(self):
         self._word_freqs = {}
 
@@ -55,10 +66,14 @@ class WordFrequencyManager(TFExercise):
             self._word_freqs[word] = 1
 
     def sorted(self):
-        return sorted(self._word_freqs.iteritems(), key=operator.itemgetter(1), reverse=True)
+        return sorted(self._word_freqs.iteritems(),
+                      key=operator.itemgetter(1), reverse=True)
 
     def info(self):
-        return super(WordFrequencyManager, self).info() + ": My major data structure is a " + self._word_freqs.__class__.__name__
+        return (super(WordFrequencyManager, self).info() +
+                ": My major data structure is a " +
+                self._word_freqs.__class__.__name__)
+
 
 class WordFrequencyController(TFExercise):
     def __init__(self, path_to_file):
@@ -74,6 +89,7 @@ class WordFrequencyController(TFExercise):
         word_freqs = self._word_freq_manager.sorted()
         for (w, c) in word_freqs[0:25]:
             print w, ' - ', c
+
 
 #
 # The main function
